48120bbff45b2ee87fd26d2012d1b940ca0eec03
Your Name <you@example.com>
2025-07-09 09:16:56 +0100

    work 09/07/2025

--- src/Form/TypechambreType.php
+++ src/Form/TypechambreType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Typechambre;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,19 +15,66 @@ class TypechambreType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('libtypechambre')
-            ->add('libtypechambre2')
-            ->add('adultesMini')
-            ->add('adultesMaxi')
-            ->add('enfantsMini')
-            ->add('enfantsMaxi')
-            ->add('bebesMaxi')
-            ->add('suppsg')
-            ->add('capacite')
-            ->add('sel')
-            ->add('abrtypechambre')
-            ->add('ordre')
-        ;
+            ->add('libtypechambre', TextType::class, [
+                'label' => 'Libellé type chambre',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('libtypechambre2', TextType::class, [
+                'label' => 'Libellé type chambre 2',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('adultesMini', IntegerType::class, [
+                'label' => 'Adultes minimum',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('adultesMaxi', IntegerType::class, [
+                'label' => 'Adultes maximum',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('enfantsMini', IntegerType::class, [
+                'label' => 'Enfants minimum',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('enfantsMaxi', IntegerType::class, [
+                'label' => 'Enfants maximum',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('bebesMaxi', IntegerType::class, [
+                'label' => 'Bébés maximum',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('suppsg', IntegerType::class, [
+                'label' => 'Supplément single',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('capacite', IntegerType::class, [
+                'label' => 'Capacité',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('sel', CheckboxType::class, [
+                'label' => 'Act',
+                'attr' => ['class' => 'form-check-input'],
+                'required' => false
+            ])
+            ->add('abrtypechambre', TextType::class, [
+                'label' => 'Abréviation type chambre',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ])
+            ->add('ordre', IntegerType::class, [
+                'label' => 'Ordre',
+                'attr' => ['class' => 'form-control'],
+                'required' => true
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
